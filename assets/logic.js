c997cd02019da2b636e3ff6a610a73e1036eaf69
Jacob <Jakelausier7878@gmail.com>
2023-06-30 15:02:39 -0400

    noted out steps need to achieve goal in js

---
+++ assets/logic.js
@@ -0,0 +1,31 @@
+// function to start quiz on the click of the start button
+
+// start and display first question
+
+// think of all the elements to target all of the answer events 
+//and the id they will have
+
+
+// logic to detect if correct answer and apply time deduction if incorrect.
+// move onto next question
+
+
+
+// loop back to question function until questtions are answered or the 
+//timer reaches 0
+
+// array of objects
+
+//Q: "question"
+//a1:
+//a2:
+//a3:
+//a4:
+//CA:
+
+
+// stop to the question function log score - 
+//read the interval value and request initials from user
+
+// write response and time remaining value to local storage
+/////////////////
