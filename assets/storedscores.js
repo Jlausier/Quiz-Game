717764d79f78540e809a979cd786b849cefd47c9
Jacob <Jakelausier7878@gmail.com>
2023-07-06 18:14:39 -0400

    fixed

--- assets/storedscores.js
+++ assets/storedscores.js
@@ -1,16 +1,46 @@
-    let initials = (JSON.parse(localStorage.getItem('username')));
-    let storedscore = (JSON.parse(localStorage.getItem('highscore')));
-    localStorage.getItem('highscore')
+let initials = (JSON.parse(localStorage.getItem('username')));
+let storedscore = (JSON.parse(localStorage.getItem('highscore')));
+    
     
     let userScore = [];
+    let yourScore = [];
     
+    const scoreList = document.getElementById('score-list')
+
     function displayScores(){
+
+        
+        userScore.push(initials)
+        yourScore.push(storedscore)
+       
+
+
+        for (let index = 0; index < userScore.length; index++) {
+            uselist= document.createElement('li')
+            uselist.innerHTML = initials + ' score : ' + storedscore
+            scoreList.appendChild(uselist)
+            
+        } 
         
 
         
     }
     displayScores()
-    
+    if (initials ===''){
+        scoreList.classList.remove('hide')
+    }
+       
     
     console.log(initials);
     console.log(storedscore);
+
+    var clearList = document.getElementById('clear')
+
+    clearList.addEventListener('click', clearAll)
+
+    function clearAll(e) {
+        localStorage.clear()
+        
+            scoreList.classList.add('hide')
+        }
+    
